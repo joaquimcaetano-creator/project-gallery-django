1c6b3ca894970e439bded6e6845d2b63a26f05be
Joaquim Caetano <pjtinha2005@gmail.com>
2026-03-28 16:51:05 -0300

    preparando deploy: procfile e whitenoise

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,23 @@
+"""
+Django settings for config project.
+"""
+
 from pathlib import Path
 import os
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-x9k8^lkmkaom**76(ae@1i@!2#1vwxuw*aum6n3d*32vb)q-px'
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+# No deploy, permitimos o endereço que o Render vai nos dar
 ALLOWED_HOSTS = ['*']
 
+# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -19,6 +30,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # ESSENCIAL PARA O DEPLOY (Servir estáticos)
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -46,6 +58,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+# Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -53,23 +66,36 @@ DATABASES = {
     }
 }
 
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+]
+
+# Internationalization
 LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 
-# --- CONFIGURAÇÃO DE ARQUIVOS ESTÁTICOS (LOGO, CSS, JS) ---
+# --- ARQUIVOS ESTÁTICOS E MÍDIA ---
+
 STATIC_URL = 'static/'
 
-# Pasta onde o Django vai "reunir" tudo para produção (Resolve o erro do collectstatic)
+# Pasta onde o Django vai reunir tudo no servidor
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# Onde o Django busca seus arquivos de desenvolvimento (Pasta que você criou)
+# Onde o Django busca seus arquivos de desenvolvimento (sua pasta da logo)
 STATICFILES_DIRS = [
     BASE_DIR / "mostra" / "static",
 ]
 
-# --- CONFIGURAÇÃO DE MÍDIA (UPLOAD DE FOTOS NO ADMIN) ---
+# Configuração para o WhiteNoise comprimir os arquivos e deixá-los mais rápidos no ar
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Configuração de Media (Imagens e PDFs que você sobe no Admin)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
